d82749559085c360e0eb5f18cb3632c8c5591b46
maheshKumar <mahii140493@gmail.com>
2026-04-12 17:53:39 +0530

    Force fix CORS middleware

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin' => 'https://tanzabooks.com',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, 
+OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, 
+Authorization',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json([], 200, $headers);
+        }
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+    }
+}
